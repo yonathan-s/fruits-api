ca05d732d910d2c9cb64dfe1a6c0ecffdf34cef0
Yonathan <yonathan.surafel62@gmail.com>
2026-08-18 11:57:26 +0100

    Env files added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const app = require("./app")
-const port = 3000
+const port = process.env.PORT
+
 
 app.listen(port, () => {
     console.log(`Example app running on port ${port}`);
